8d03980027192de73845076e929ade1939db468b
Denys <denis.kravchenko@gmail.com>
2023-11-06 16:13:51 -0800

    add gpt-4 turbo

--- types/openai.ts
+++ types/openai.ts
@@ -11,6 +11,7 @@ export enum OpenAIModelID {
   GPT_3_5 = 'gpt-3.5-turbo-16k',
   GPT_3_5_AZ = 'gpt-35-turbo',
   GPT_4 = 'gpt-4',
+  GPT_4_TURBO = 'gpt-4-1106-preview',
   GPT_4_32K = 'gpt-4-32k',
 }
 
@@ -35,6 +36,13 @@ export const OpenAIModels: Record<OpenAIModelID, OpenAIModel> = {
     name: 'GPT-4',
     maxLength: 24000,
     tokenLimit: 8000,
+  },
+
+  [OpenAIModelID.GPT_4_TURBO]: {
+    id: OpenAIModelID.GPT_4_TURBO,
+    name: 'GPT-4-turbo',
+    maxLength: 96000,
+    tokenLimit: 128000,
   },
   [OpenAIModelID.GPT_4_32K]: {
     id: OpenAIModelID.GPT_4_32K,
